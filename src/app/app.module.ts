8d3b02a09f3886019fa539021595996812da3a4b
Palveet <palveetkaursaluja@gmail.com>
2020-07-11 20:03:20 +0530

    shipping added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ShippingComponent } from './shipping/shipping.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       {path:'cart',component:CartComponent},
+      {path:'shipping',component:ShippingComponent},
     ])
   ],
   declarations: [
